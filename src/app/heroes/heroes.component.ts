7ebf1c7a9784f126f9dfe29753202ef1d0635485
noesterle <noesterle1@gmail.com>
2020-03-22 14:17:28 -0400

    Moved hero details to it's own webpage, with the url based on the id.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,20 +10,12 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
   heroes: Hero[];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}, name=${hero.name}`);
   }
 
   getHeroes(): void {
